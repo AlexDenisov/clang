f3f6520a5d75caac16b0f3b3a14b694ecb5437e2
AlexDenisov <1101.debian@gmail.com>
2015-03-29 12:13:33 +0200

    Add CG tests for objc_boxable-mac-arc

--- test/CodeGenObjC/nsvalue-boxed-expressions-mac-arc.m
+++ test/CodeGenObjC/nsvalue-boxed-expressions-mac-arc.m
@@ -4,31 +4,24 @@
 
 // CHECK:      [[CLASS:@.*]]        = external global %struct._class_t
 // CHECK:      [[NSVALUE:@.*]]      = {{.*}}[[CLASS]]{{.*}}
-
-// CHECK:      [[METH:@.*]]            = private global{{.*}}valueWithRange:{{.*}}
-// CHECK-NEXT: [[RANGE_SEL:@.*]]       = {{.*}}[[METH]]{{.*}}
-// CHECK:      [[METH:@.*]]            = private global{{.*}}valueWithPoint:{{.*}}
-// CHECK-NEXT: [[POINT_SEL:@.*]]       = {{.*}}[[METH]]{{.*}}
-// CHECK:      [[METH:@.*]]            = private global{{.*}}valueWithSize:{{.*}}
-// CHECK-NEXT: [[SIZE_SEL:@.*]]        = {{.*}}[[METH]]{{.*}}
-// CHECK:      [[METH:@.*]]            = private global{{.*}}valueWithRect:{{.*}}
-// CHECK-NEXT: [[RECT_SEL:@.*]]        = {{.*}}[[METH]]{{.*}}
-// CHECK:      [[METH:@.*]]            = private global{{.*}}valueWithEdgeInsets:{{.*}}
-// CHECK-NEXT: [[EDGE_INSETS_SEL:@.*]] = {{.*}}[[METH]]{{.*}}
+// CHECK:      [[RANGE_STR:.*]]     = {{.*}}_NSRange=QQ{{.*}}
+// CHECK:      [[METH:@.*]]         = private global{{.*}}valueWithBytes:objCType:{{.*}}
+// CHECK:      [[VALUE_SEL:@.*]]    = {{.*}}[[METH]]{{.*}}
+// CHECK:      [[POINT_STR:.*]]     = {{.*}}_NSPoint=dd{{.*}}
+// CHECK:      [[SIZE_STR:.*]]      = {{.*}}_NSSize=dd{{.*}}
+// CHECK:      [[RECT_STR:.*]]      = {{.*}}_NSRect={_NSPoint=dd}{_NSSize=dd}}{{.*}}
+// CHECK:      [[EDGE_STR:.*]]      = {{.*}}NSEdgeInsets=dddd{{.*}}
 
 // CHECK-LABEL: define void @doRange()
 void doRange() {
-  // CHECK:      [[RANGE:%.*]]     = bitcast %struct._NSRange* {{.*}}
-  // CHECK:      [[RECV_PTR:%.*]]  = load {{.*}} [[NSVALUE]]
-  // CHECK:      [[SEL:%.*]]       = load i8** [[RANGE_SEL]]
-  // CHECK:      [[RECV:%.*]]      = bitcast %struct._class_t* [[RECV_PTR]] to i8*
-  // CHECK:      [[RANGE_PTR:%.*]] = bitcast %struct._NSRange* {{.*}}
-  // CHECK-NEXT: [[P1_PTR:%.*]]    = getelementptr {{.*}} [[RANGE_PTR]], i32 0, i32 0
-  // CHECK-NEXT: [[P1:%.*]]        = load i64* [[P1_PTR]], align 1
-  // CHECK-NEXT: [[P2_PTR:%.*]]    = getelementptr {{.*}} [[RANGE_PTR]], i32 0, i32 1
-  // CHECK-NEXT: [[P2:%.*]]        = load i64* [[P2_PTR]], align 1
+  // CHECK:      [[RANGE:%.*]]      = bitcast %struct._NSRange* {{.*}}
+  // CHECK:      call void @llvm.memcpy{{.*}}[[RANGE]]{{.*}}
+  // CHECK:      [[RECV_PTR:%.*]]   = load {{.*}} [[NSVALUE]]
+  // CHECK:      [[RANGE_CAST:%.*]] = bitcast %struct._NSRange* {{.*}}
+  // CHECK:      [[SEL:%.*]]        = load i8*, i8** [[VALUE_SEL]]
+  // CHECK:      [[RECV:%.*]]       = bitcast %struct._class_t* [[RECV_PTR]] to i8*
   NSRange ns_range = { .location = 0, .length = 42 };
-  // CHECK:      call {{.*objc_msgSend.*}}(i8* [[RECV]], i8* [[SEL]], i64 [[P1]], i64 [[P2]])
+  // CHECK:      call {{.*objc_msgSend.*}}(i8* [[RECV]], i8* [[SEL]], i8* [[RANGE_CAST]], i8* {{.*}}[[RANGE_STR]]{{.*}})
   // CHECK:      call i8* @objc_retainAutoreleasedReturnValue
   NSValue *range = @(ns_range);
   // CHECK:      call void @objc_release
@@ -37,17 +30,14 @@ void doRange() {
 
 // CHECK-LABEL: define void @doPoint()
 void doPoint() {
-  // CHECK:      [[POINT:%.*]]     = bitcast %struct._NSPoint* {{.*}}
-  // CHECK:      [[RECV_PTR:%.*]]  = load {{.*}} [[NSVALUE]]
-  // CHECK:      [[SEL:%.*]]       = load i8** [[POINT_SEL]]
-  // CHECK:      [[RECV:%.*]]      = bitcast %struct._class_t* [[RECV_PTR]] to i8*
-  // CHECK:      [[POINT_PTR:%.*]] = bitcast %struct._NSPoint* {{.*}}
-  // CHECK-NEXT: [[P1_PTR:%.*]]    = getelementptr {{.*}} [[POINT_PTR]], i32 0, i32 0
-  // CHECK-NEXT: [[P1:%.*]]        = load double* [[P1_PTR]], align 1
-  // CHECK-NEXT: [[P2_PTR:%.*]]    = getelementptr {{.*}} [[POINT_PTR]], i32 0, i32 1
-  // CHECK-NEXT: [[P2:%.*]]        = load double* [[P2_PTR]], align 1
+  // CHECK:      [[POINT:%.*]]      = bitcast %struct._NSPoint* {{.*}}
+  // CHECK:      call void @llvm.memcpy{{.*}}[[POINT]]{{.*}}
+  // CHECK:      [[RECV_PTR:%.*]]   = load {{.*}} [[NSVALUE]]
+  // CHECK:      [[POINT_CAST:%.*]] = bitcast %struct._NSPoint* {{.*}}
+  // CHECK:      [[SEL:%.*]]        = load i8*, i8** [[VALUE_SEL]]
+  // CHECK:      [[RECV:%.*]]       = bitcast %struct._class_t* [[RECV_PTR]] to i8*
   NSPoint ns_point = { .x = 42, .y = 24 };
-  // CHECK:      call {{.*objc_msgSend.*}}(i8* [[RECV]], i8* [[SEL]], double [[P1]], double [[P2]])
+  // CHECK:      call {{.*objc_msgSend.*}}(i8* [[RECV]], i8* [[SEL]], i8* [[POINT_CAST]], i8* {{.*}}[[POINT_STR]]{{.*}})
   // CHECK:      call i8* @objc_retainAutoreleasedReturnValue
   NSValue *point = @(ns_point);
   // CHECK:      call void @objc_release
@@ -56,17 +46,14 @@ void doPoint() {
 
 // CHECK-LABEL: define void @doSize()
 void doSize() {
-  // CHECK:      [[SIZE:%.*]]     = bitcast %struct._NSSize* {{.*}}
-  // CHECK:      [[RECV_PTR:%.*]] = load {{.*}} [[NSVALUE]]
-  // CHECK:      [[SEL:%.*]]      = load i8** [[SIZE_SEL]]
-  // CHECK:      [[RECV:%.*]]     = bitcast %struct._class_t* [[RECV_PTR]] to i8*
-  // CHECK:      [[SIZE_PTR:%.*]] = bitcast %struct._NSSize* {{.*}}
-  // CHECK-NEXT: [[P1_PTR:%.*]]   = getelementptr {{.*}} [[SIZE_PTR]], i32 0, i32 0
-  // CHECK-NEXT: [[P1:%.*]]       = load double* [[P1_PTR]], align 1
-  // CHECK-NEXT: [[P2_PTR:%.*]]   = getelementptr {{.*}} [[SIZE_PTR]], i32 0, i32 1
-  // CHECK-NEXT: [[P2:%.*]]       = load double* [[P2_PTR]], align 1
+  // CHECK:      [[SIZE:%.*]]      = bitcast %struct._NSSize* {{.*}}
+  // CHECK:      call void @llvm.memcpy{{.*}}[[SIZE]]{{.*}}
+  // CHECK:      [[RECV_PTR:%.*]]  = load {{.*}} [[NSVALUE]]
+  // CHECK:      [[SIZE_CAST:%.*]] = bitcast %struct._NSSize* {{.*}}
+  // CHECK:      [[SEL:%.*]]       = load i8*, i8** [[VALUE_SEL]]
+  // CHECK:      [[RECV:%.*]]      = bitcast %struct._class_t* [[RECV_PTR]] to i8*
   NSSize ns_size = { .width = 42, .height = 24 };
-  // CHECK:      call {{.*objc_msgSend.*}}(i8* [[RECV]], i8* [[SEL]], double [[P1]], double [[P2]])
+  // CHECK:      call {{.*objc_msgSend.*}}(i8* [[RECV]], i8* [[SEL]], i8* [[SIZE_CAST]], i8* {{.*}}[[SIZE_STR]]{{.*}})
   // CHECK:      call i8* @objc_retainAutoreleasedReturnValue
   NSValue *size = @(ns_size);
   // CHECK:      call void @objc_release
@@ -75,14 +62,15 @@ void doSize() {
 
 // CHECK-LABEL: define void @doRect()
 void doRect() {
-  // CHECK:      [[RECT:%.*]]     = alloca %struct._NSRect{{.*}}
-  // CHECK:      [[RECV_PTR:%.*]] = load {{.*}} [[NSVALUE]]
-  // CHECK:      [[SEL:%.*]]      = load i8** [[RECT_SEL]]
-  // CHECK:      [[RECV:%.*]]     = bitcast %struct._class_t* [[RECV_PTR]] to i8*
+  // CHECK:      [[RECT:%.*]]      = alloca %struct._NSRect{{.*}}
+  // CHECK:      [[RECV_PTR:%.*]]  = load {{.*}} [[NSVALUE]]
+  // CHECK:      [[RECT_CAST:%.*]] = bitcast %struct._NSRect* [[RECT]] to i8*
+  // CHECK:      [[SEL:%.*]]       = load i8*, i8** [[VALUE_SEL]]{{.*}}
+  // CHECK:      [[RECV:%.*]]      = bitcast %struct._class_t* [[RECV_PTR]] to i8*
   NSPoint ns_point = { .x = 42, .y = 24 };
   NSSize ns_size = { .width = 42, .height = 24 };
   NSRect ns_rect = { .origin = ns_point, .size = ns_size };
-  // CHECK:      call {{.*objc_msgSend.*}}(i8* [[RECV]], i8* [[SEL]], %struct._NSRect* byval align 8 [[RECT]])
+  // CHECK:      call {{.*objc_msgSend.*}}(i8* [[RECV]], i8* [[SEL]], i8* [[RECT_CAST]], i8*{{.*}}[[RECT_STR]]{{.*}})
   // CHECK:      call i8* @objc_retainAutoreleasedReturnValue
   NSValue *rect = @(ns_rect);
   // CHECK:      call void @objc_release
@@ -91,12 +79,13 @@ void doRect() {
 
 // CHECK-LABEL: define void @doNSEdgeInsets()
 void doNSEdgeInsets() {
-  // CHECK:      [[EDGE_INSETS:%.*]] = alloca %struct.NSEdgeInsets{{.*}}
-  // CHECK:      [[RECV_PTR:%.*]]    = load {{.*}} [[NSVALUE]]
-  // CHECK:      [[SEL:%.*]]         = load i8** [[EDGE_INSETS_SEL]]
-  // CHECK:      [[RECV:%.*]]        = bitcast %struct._class_t* [[RECV_PTR]] to i8*
+  // CHECK:      [[EDGE:%.*]]      = alloca %struct.NSEdgeInsets{{.*}}
+  // CHECK:      [[RECV_PTR:%.*]]  = load {{.*}} [[NSVALUE]]
+  // CHECK:      [[EDGE_CAST:%.*]] = bitcast %struct.NSEdgeInsets* {{.*}}
+  // CHECK:      [[SEL:%.*]]       = load i8*, i8** [[VALUE_SEL]]
+  // CHECK:      [[RECV:%.*]]      = bitcast %struct._class_t* [[RECV_PTR]] to i8*
   NSEdgeInsets ns_edge_insets;
-  // CHECK:      call {{.*objc_msgSend.*}}(i8* [[RECV]], i8* [[SEL]], %struct.NSEdgeInsets* byval align 8 [[EDGE_INSETS]])
+  // CHECK:      call {{.*objc_msgSend.*}}(i8* [[RECV]], i8* [[SEL]], i8* [[EDGE_CAST]], i8*{{.*}}[[EDGE_STR]]{{.*}})
   // CHECK:      call i8* @objc_retainAutoreleasedReturnValue
   NSValue *edge_insets = @(ns_edge_insets);
   // CHECK:      call void @objc_release
